82ddb23e94b989e35cad9d611c30b8719c229c03
StefanNick <stefanidi.n@gmail.com>
2025-11-18 16:21:22 +0300

    Add files via upload

--- connect_to_s3.py
+++ connect_to_s3.py
@@ -5,6 +5,72 @@ from pathlib import Path
 import glob
 import psycopg2
 import json
+import pprint
+
+version_list = [
+    {
+        'version': '1',
+        "answers": [
+      {
+        "question": "Откуда вы узнали о нашем сервисе?",
+        "options": {}
+      },
+      {
+        "question": "Ваш опыт торговли на маркетплейсах?",
+        "options": {
+          "1": "Менее 1 года",
+          "2": "1 - 2 года",
+          "3": "3 - 5 лет",
+          "4": "Более 5 лет"
+        }
+      },
+      {
+        "question": "На скольких маркетплейсах вы ведете торговлю?",
+        "options": {
+          "1": "1",
+          "2": "2",
+          "3": "3",
+          "4": "Более 3"
+        }
+      },
+      {
+        "question": "Вы можете описать своими словами, как вы поняли, для чего нужен наш сервис?",
+        "options": {}
+      },
+      {
+        "question": "Какие преимущества сервиса вы отметили, насколько они важны для вас?",
+        "options": {}
+      },
+      {
+        "question": "Возникло ли у вас желание воспользоваться сервисом?",
+        "options": {
+          "1": "Да",
+          "2": "Нет"
+        }
+      },
+      {
+        "question": "Поясните, пожалуйста, почему вы приняли в предыдущем вопросе такое решение?",
+        "options": {}
+      },
+      {
+        "question": "Что мы могли бы добавить в сервис, чтобы у вас возникло желание им воспользоваться?",
+        "options": {}
+      },
+      {
+        "question": "Какими сервисами (аналитическими, логистическими и т.д.) вы сейчас пользуетесь?",
+        "options": {}
+      }
+    ]
+    }
+]
+
+
+
+
+
+
+
+
 
 access_key = 'dp-prod-interview-user'
 secret_key = 'kyJrYJ0T2O7UVv3q'
@@ -82,22 +148,43 @@ print(res)
 
 
 
+def serilalize_data(path_to_folder):
+    processed_data = []
+    for file in path_to_folder.glob('*.JSON'):
+        
+        try:
+            with open(file, 'r',  encoding='Utf-8') as interview_file:
+                interview_data = json.load(interview_file)
+                #print(interview_data[0])
+                if  isinstance(interview_data[0], dict) and interview_data[0].get('seller_id'):
+                    pprint.pprint(interview_data[0]['seller_id'] )
+                    answer_dict ={}
+                    for i in interview_data[0]['answers']:
+                        answer_dict_elem = {
+                            i["question"] : i['value']
+                            }
+                        answer_dict.update(answer_dict_elem)
+                    processed_data_elem = {
+                        'seller_id': interview_data[0].get('seller_id'),
+                        'version': interview_data[0].get('version'),
+                        'answers' : answer_dict
+                        }
+                    processed_data.append(processed_data_elem)
+        except FileNotFoundError:
+            print("Error: The specified JSON file was not found.")
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON from file: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+    return processed_data
 
-for file in path.glob('*.JSON'):
-    try:
-        with open(file, 'r',  encoding='Utf-8') as interview_file:
-            interview_data = json.load(interview_file)
-    except FileNotFoundError:
-        print("Error: The specified JSON file was not found.")
-        interview_data = {} # Or handle as appropriate
-    except json.JSONDecodeError as e:
-        print(f"Error decoding JSON from file: {e}")
-        interview_data = {} # Or handle as appropriate
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        interview_data = {} # Or handle as appropriate
+res2 = serilalize_data(path)
 
-    print(interview_data)
+print(res2)
 
 
-
+my_dict = {'apple': 1, 'banana': 2}
+value = my_dict.get('banana') # вернет 2
+value_not_exist = my_dict.get('grape', 'Ключа нет') # вернет 'Ключа нет'
+print(value)
+print(value_not_exist)
